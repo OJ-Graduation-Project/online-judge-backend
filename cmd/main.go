9c2baa9d51d93dc5d549318cde01e03d56cfb21e
Omar Radwan <omaradwanx@gmail.com>
2021-12-28 22:25:37 +0200

    Create Configuration Files
    * Add application configuration in config.json
    * Add database configuration in .env-example
    * Add methods to read both configuration files

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	
+	"flag"
+	"github.com/OJ-Graduation-Project/online-judge-backend/config"
 )
 
 func main() {
+
+	// env file path
+	var envFilePath string
+
+	flag.StringVar(
+		&envFilePath,
+		"env",
+		"./config/res/.env-example",
+		"Location of the environment file",
+	)
 	
-	
+
+
+	// config file flag
+	var configFilePath string
+	flag.StringVar(
+		&configFilePath,
+		"config",
+		"./config/res/config.json",
+		"Location of the config file",
+	)
+
+	flag.Parse()
+
+
+	config.LoadEnv(envFilePath)
+	config := config.LoadConfig(configFilePath)
+
+
+	_ = config // will be remove later
 }
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/OJ-Graduation-Project/online-judge-backend
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0
