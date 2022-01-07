f58c779cdb3940d3b675d7c7714a77883d69059b
Omar Radwan <omaradwanx@gmail.com>
2022-01-07 19:32:05 +0200

    Add correctness test for scoreboard

--- internal/contest/contest_test.go
+++ internal/contest/contest_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"testing"
 )
 
@@ -52,5 +53,61 @@ func TestContest(t *testing.T) {
 	fmt.Println(slowRanks)
 	if fastRanks != slowRanks {
 		t.Fail()
+	}
+}
+
+func TestCorrectness(t *testing.T) {
+	var slow Contest = Contest{}
+	var fast Contest = Contest{}
+
+	const LENGTH = 100
+	const WRONG_SUBMISSION_COST = 1
+	const SCORE_DIFF = 1
+
+	slow.RegisteredUserIds = make([]int, LENGTH)
+	slow.ContestProblemIds = make([]int, LENGTH)
+	slow.ProblemsScore = make([]int, LENGTH)
+	slow.WrongSubmissionCost = WRONG_SUBMISSION_COST
+
+	fast.RegisteredUserIds = make([]int, LENGTH)
+	fast.ContestProblemIds = make([]int, LENGTH)
+	fast.ProblemsScore = make([]int, LENGTH)
+	fast.WrongSubmissionCost = WRONG_SUBMISSION_COST
+
+	for i := 1; i <= LENGTH; i++ {
+		slow.RegisteredUserIds[i-1] = i
+		slow.ContestProblemIds[i-1] = i
+		slow.ProblemsScore[i-1] = i * SCORE_DIFF
+
+		fast.RegisteredUserIds[i-1] = i
+		fast.ContestProblemIds[i-1] = i
+		fast.ProblemsScore[i-1] = i * SCORE_DIFF
+
+	}
+
+	slow.Start("slow")
+	fast.Start("fast")
+
+	for i := 0; i <= 2*LENGTH*LENGTH; i++ {
+		op := rand.Intn(2)
+		userId := 1 + rand.Intn(LENGTH-1)
+		problemId := 1 + rand.Intn(LENGTH)
+		switch op {
+
+		case 0:
+			slow.AcceptedSubmission(userId, problemId)
+			fast.AcceptedSubmission(userId, problemId)
+
+		case 1:
+			fast.WrongSubmission(userId, problemId)
+			slow.WrongSubmission(userId, problemId)
+		}
+
+		if slow.DisplayAllRanks() != fast.DisplayAllRanks() {
+			fmt.Println(slow.DisplayAllRanks())
+			fmt.Println(fast.DisplayAllRanks())
+			t.Fail()
+			break
+		}
 	}
 }
